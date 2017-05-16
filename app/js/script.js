62dbd8e81ee4b78def1c99d4cf945d21d72b1601
Oleg <yastreb92@inbox.ru>
2017-05-16 15:15:28 +0300

    gallery

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,32 @@
 $(document).ready(function () {
 
+  $('.slider-for').slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+    arrows: true,
+   fade: true,
+   asNavFor: '.slider-nav'
+ });
+ $('.slider-nav').slick({
+   slidesToShow: 4,
+   centerPadding: false,
+   slidesToScroll: 1,
+   asNavFor: '.slider-for',
+   dots: false,
 
+   centerMode: true,
+   focusOnSelect: true
+ });
+
+ /*popup*/
+ $('.image-popup-vertical-fit').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		mainClass: 'mfp-img-mobile',
+		image: {
+			verticalFit: true
+		}
+
+	});
+ /*popup*/
 });
